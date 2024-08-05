ff2613b5e245e06de6cb194f576bd88906b36598
Or <orwolf132@gmail.com>
2024-08-05 16:08:08 +0300

    Asignment2

--- Asignment2.C
+++ Asignment2.C
@@ -2,16 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-int initArray(int *arr, int size){
-
-    
+void freeMemory(int *arr){
+    free(arr);
 }
 
-int createArray(int size){
+
+int doubleArraySize(int **ptr,int size){
+    int newSize=size*2;
+    int *temp=(int*)realloc(*ptr,newSize*sizeof(int));
+   if(temp==NULL){
+    return -1;
+   }
+    *ptr=temp;
+    for(int i=size;i<newSize;i++){
+        (*ptr)[i]=0;
+    }
+}
+
+void printArr(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void initArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = i + 1;
+    }
+}
+
+int createArray(int size)
+{
     int *p;
-    p=(int*)malloc(size*sizeof(int));
-    if(p==NULL){
+    p = (int *)malloc(size * sizeof(int));
+    if (p == NULL)
+    {
         return NULL;
     }
     return *p;
